89580004627f1deff56878eb460e0c5d39a18cd8
remmerw <remmer.wilts@gmail.com>
2021-01-21 10:26:10 +0100

    Version 2.0.8

--- app/src/main/java/threads/server/core/events/Event.java
+++ app/src/main/java/threads/server/core/events/Event.java
@@ -4,10 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-import androidx.room.TypeConverter;
-import androidx.room.TypeConverters;
-
-import java.util.Date;
 
 @Entity
 public class Event {
@@ -19,45 +15,23 @@ public class Event {
     @NonNull
     @ColumnInfo(name = "content")
     private final String content;
-    @NonNull
-    @TypeConverters(Event.class)
-    @ColumnInfo(name = "date")
-    private final Date date;
 
-    Event(@NonNull String identifier, @NonNull String content, @NonNull Date date) {
+    Event(@NonNull String identifier, @NonNull String content) {
 
         this.identifier = identifier;
-        this.date = date;
         this.content = content;
     }
 
     public static Event createEvent(@NonNull String identifier, @NonNull String content) {
-        return new Event(identifier, content, new Date());
+        return new Event(identifier, content);
     }
 
-    @TypeConverter
-    public static Date toDate(Long value) {
-        return value == null ? null : new Date(value);
-    }
-
-    @TypeConverter
-    public static Long toLong(Date date) {
-        if (date == null) {
-            return null;
-        } else {
-            return date.getTime();
-        }
-    }
 
     @NonNull
     public String getContent() {
         return content;
     }
 
-    @NonNull
-    public Date getDate() {
-        return date;
-    }
 
     @NonNull
     public String getIdentifier() {
